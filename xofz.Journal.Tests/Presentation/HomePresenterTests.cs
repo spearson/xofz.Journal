f2e7ac8c8aabd7ceace581df1a7ad4d94f07d72a
Sam Pearson <sam@xofz.com>
2017-01-25 07:34:26 -0800

    Add some tests

---
+++ xofz.Journal.Tests/Presentation/HomePresenterTests.cs
@@ -0,0 +1,93 @@
+﻿namespace xofz.Journal.Tests.Presentation
+{
+    using System;
+    using System.Linq;
+    using FakeItEasy;
+    using xofz.Framework;
+    using xofz.Journal.Framework;
+    using xofz.Journal.Presentation;
+    using xofz.Journal.UI;
+    using xofz.Presentation;
+    using Xunit;
+
+    public class HomePresenterTests
+    {
+        public class Context
+        {
+            protected Context()
+            {
+                this.ui = A.Fake<HomeUi>();
+                this.web = new MethodWeb();
+                this.navigator = A.Fake<Navigator>();
+                this.web.RegisterDependency(this.navigator);
+
+                this.loader = A.Fake<JournalEntryLoader>();
+                this.saver = A.Fake<JournalEntrySaver>();
+                this.web.RegisterDependency(this.loader);
+                this.web.RegisterDependency(this.saver);
+                this.presenter = new HomePresenter(this.ui, this.web);
+            }
+
+            protected readonly HomeUi ui;
+            protected readonly MethodWeb web;
+            protected readonly Navigator navigator;
+            protected readonly JournalEntryLoader loader;
+            protected readonly JournalEntrySaver saver;
+            protected readonly HomePresenter presenter;
+        }
+
+        public class When_Setup_is_called : Context
+        {
+            [Fact]
+            public void Registers_itself_with_the_navigator()
+            {
+                this.presenter.Setup();
+
+                A.CallTo(() => this.navigator.RegisterPresenter(this.presenter))
+                    .MustHaveHappened();
+            }
+        }
+
+        public class When_Start_is_called : Context
+        {
+            [Fact]
+            public void Calls_loader_Load()
+            {
+                this.presenter.Start();
+
+                A.CallTo(() => this.loader.Load()).MustHaveHappened();
+            }
+
+            [Fact]
+            public void Orders_entries_by_modified_timestamp_descending()
+            {
+                var entry1 = new JournalEntry { ModifiedTimestamp = DateTime.MinValue };
+                var entry2 = new JournalEntry { ModifiedTimestamp = DateTime.Now };
+                A.CallTo(() => this.loader.Load())
+                    .Returns(
+                        new[]
+                        {
+                            entry1,
+                            entry2
+                        });
+                this.presenter.Start();
+                Assert.Equal(entry2, this.ui.Entries.First());
+            }
+
+            [Fact]
+            public void Raises_the_EntrySelected_event_on_the_UI()
+            {
+                var er = A.Fake<EventRaiser>();
+                this.web.RegisterDependency(er);
+
+                this.presenter.Start();
+
+                A.CallTo(() => er.Raise(this.ui, "EntrySelected", 0)).MustHaveHappened();
+            }
+        }
+
+        public class When_the_new_key_is_tapped : Context
+        {
+        }
+    }
+}
---
+++ xofz.Journal.Tests/Root/Commands/SetupHomeCommandTests.cs
@@ -0,0 +1,39 @@
+﻿namespace xofz.Journal.Tests.Root.Commands
+{
+    using FakeItEasy;
+    using xofz.Framework;
+    using xofz.Journal.Framework;
+    using xofz.Journal.Root.Commands;
+    using xofz.Journal.UI;
+    using Xunit;
+
+    public class SetupHomeCommandTests
+    {
+        public class Context
+        {
+            protected Context()
+            {
+                this.web = A.Fake<MethodWeb>();
+                this.command = new SetupHomeCommand(
+                    A.Fake<HomeUi>(), 
+                    this.web);
+            }
+            protected readonly MethodWeb web;
+            protected readonly SetupHomeCommand command;
+        }
+
+        public class When_Execute_is_called : Context
+        {
+            [Fact]
+            public void Registers_a_JournalEntryManager()
+            {
+                this.command.Execute();
+
+                A.CallTo(() => this.web.RegisterDependency(
+                    A<JournalEntryManager>.Ignored,
+                    null))
+                    .MustHaveHappened();
+            }
+        }
+    }
+}
